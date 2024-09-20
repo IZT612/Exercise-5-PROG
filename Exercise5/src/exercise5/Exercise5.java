e07ddef2a0162b3e3509c4ce4b0667ac5bdc66eb
iziva <iziva@DESKTOP-JERP5OE>
2024-09-20 19:38:54 +0200

    Publicacion de la tarea

---
+++ Exercise5/src/exercise5/Exercise5.java
@@ -0,0 +1,12 @@
+package exercise5;
+
+public class Exercise5 {
+
+	public static void main(String[] args) {
+		System.out.println("Name: Iván Zamora Torres");
+		System.out.println("Address: Urb/ Pradollano, city: Seville");
+		System.out.println("Phone number: +34 643 41 93 84");
+
+	}
+
+}
